9b07c222b13223931ad2acfea28c7dae265c25f9
ramya1631 <ramya.koluguri16@gmail.com>
2025-07-16 19:18:08 -0500

    Completed the code for repositories

--- BackEnd/src/main/java/com/example/chatbot/project/controllers/AdminController.java
+++ BackEnd/src/main/java/com/example/chatbot/project/controllers/AdminController.java
@@ -1,6 +1,9 @@
 package com.example.chatbot.project.controllers;
 
+import com.example.chatbot.project.repositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 
  @Autowired
         private AdminRepository adminRepository;
-
-private  final String adminEndpoint="adminEndpoint = ("/admin") ;
+ //GET the full list of admins
+        //Endpoint is
+@GetMapping
+        public String getAdminPage() {
+            return "Welcome to the Admin Page!";
+        }
+@PostMapping("/add")
+        public String addAdmin() {
+            // Logic to add an admin
+            return "Admin added successfully!";
+        }
 
 
     }
--- BackEnd/src/main/java/com/example/chatbot/project/repositories/AdminRepository.java
+++ BackEnd/src/main/java/com/example/chatbot/project/repositories/AdminRepository.java
@@ -3,8 +3,6 @@ package com.example.chatbot.project.repositories;
 import com.example.chatbot.project.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
-public interface AdminRespository  extends JpaRepository<Admin, Long> {
+public interface AdminRepository  extends JpaRepository<Admin, Long> {
 
 }
