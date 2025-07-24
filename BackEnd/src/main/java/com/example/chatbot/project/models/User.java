2c7a0e29fc45f3e4a30c5b0b3f7e234b95aadad8
ramya1631 <ramya.koluguri16@gmail.com>
2025-07-24 02:39:58 -0500

    Testing the Endpoints in postman

--- BackEnd/src/main/java/com/example/chatbot/project/models/User.java
+++ BackEnd/src/main/java/com/example/chatbot/project/models/User.java
@@ -48,6 +48,7 @@ public class User {
         return email;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
--- BackEnd/src/main/java/com/example/chatbot/project/repositories/AdminRepository.java
+++ BackEnd/src/main/java/com/example/chatbot/project/repositories/AdminRepository.java
@@ -6,3 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AdminRepository  extends JpaRepository<Admin, Long> {
 
 }
+
