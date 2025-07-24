07e5d1cb3dc156a5b985ef8bdaf205e10a74059e
ramya1631 <ramya.koluguri16@gmail.com>
2025-07-24 02:27:59 -0500

    Configure CORS for frontend integration

--- BackEnd/src/main/java/com/example/chatbot/project/controllers/ContactController.java
+++ BackEnd/src/main/java/com/example/chatbot/project/controllers/ContactController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:5173")
 
 
-
 public class ContactController {
     @Autowired
     private ContactRepository contactRepository;
