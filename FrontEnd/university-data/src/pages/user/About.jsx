f7490491bcfdfa3b1f45131548b3a5b03438a391
ramya1631 <ramya.koluguri16@gmail.com>
2025-07-29 16:41:07 -0500

    Add About page describing app purpose and background

---
+++ FrontEnd/university-data/src/pages/user/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="text-center mt-5">
+      <h2>About Us</h2>
+      <p>University information goes here.</p>
+    </div>
+  );
+};
+
+export default About;
