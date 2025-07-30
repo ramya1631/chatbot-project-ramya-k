226976ae5ec01a0fbe43874e581862e37daea57f
ramya1631 <ramya.koluguri16@gmail.com>
2025-07-30 13:43:54 -0500

    Set up App.jsx with routes and created App.css file

--- FrontEnd/university-data/src/App.jsx
+++ FrontEnd/university-data/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <div className="App">
           <Navbar />
           <main>
+
             <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
